5761b7e21b950d1bddc9269acad191df20d2b646
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2025-06-22 23:39:41 -0400

    mre

--- App.tsx
+++ App.tsx
@@ -1,9 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { AudioPro } from "react-native-audio-pro";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Button
+        title="play"
+        onPress={() => {
+          AudioPro.play({
+            id: "remote-track-1",
+            url: "https://rnap.dev/audio-soundhelix-song-1-tschurger.mp3",
+            title: "Soundhelix Song 1",
+            artwork: "https://rnap.dev/artwork-usgs-bAji8qv_LlY-unsplash.jpg",
+            artist: "T. Schurger",
+          });
+        }}
+      />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
